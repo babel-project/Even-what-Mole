c61f615586d4d41c7718b555b1ff19c8d35b0fd6
Hiroyuki Sasagawa <hs19870702@gmail.com>
2014-11-09 17:36:00 +0900

    created function that get image by HttpRequest.

---
+++ Classes/BabelHttpRequest.cpp
@@ -0,0 +1,9 @@
+//
+//  BabelHttpRequest.cpp
+//  Even-what-Mole
+//
+//  Created by 笹川 博幸 on 2014/10/22.
+//
+//
+
+#include "BabelHttpRequest.h"
---
+++ Classes/BabelHttpRequest.h
@@ -0,0 +1,14 @@
+//
+//  BabelHttpRequest.h
+//  Even-what-Mole
+//
+//  Created by 笹川 博幸 on 2014/10/22.
+//
+//
+
+#ifndef __Even_what_Mole__BabelHttpRequest__
+#define __Even_what_Mole__BabelHttpRequest__
+
+#include <stdio.h>
+
+#endif /* defined(__Even_what_Mole__BabelHttpRequest__) */
